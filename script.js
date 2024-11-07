3ac0ec89016d611e574976fcd2fc7e6d12436b03
StilettoDagger <159158452+StilettoDagger@users.noreply.github.com>
2024-11-07 21:01:40 +0300

    Add displayBooks function that displays all books in the library array as cards on the DOM

--- script.js
+++ script.js
@@ -13,7 +13,37 @@ function addBookToLibrary(name, author, publishDate, pages) {
     myLibrary.push(book);
 }
 
+function displayBooks() {
+    const libraryContainer = document.getElementById("library-container");
+    for (const book of myLibrary) {
+
+        const bookCard = document.createElement("div");
+        bookCard.classList.add("book-card");
+
+        const bookTitle = document.createElement("h2");
+        bookTitle.textContent = book.name;
+        bookCard.appendChild(bookTitle);
+
+        const bookAuthor = document.createElement("p");
+        bookAuthor.innerHTML = `<strong>Author: </strong> ${book.author}`;
+        bookCard.appendChild(bookAuthor);
+
+        const bookPublishDate = document.createElement("p");
+        bookPublishDate.innerHTML = `<strong>Publish Date: </strong> ${book.publishDate}`;
+        bookCard.appendChild(bookPublishDate);
+
+        const bookPages = document.createElement("p");
+        bookPages.innerHTML = `<strong>Page Count: </strong> ${book.pages}`;
+        bookCard.appendChild(bookPages);
+
+
+        libraryContainer.appendChild(bookCard);
+    }
+}
+
 addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 1937, 310);
 addBookToLibrary("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 1997, 223);
 addBookToLibrary("Dune", "Frank Herbert", 1965, 412);
+
+displayBooks();
 
